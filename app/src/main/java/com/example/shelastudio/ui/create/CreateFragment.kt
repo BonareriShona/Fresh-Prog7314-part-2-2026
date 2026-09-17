b0f3b1ec60657ebe09f97ad720b3a5869e2a227e
Fanelo Maluleke <st10445045@imconnect.edu.za>
2026-09-17 17:16:13 +0200

    Create CreateFragment.kt

---
+++ app/src/main/java/com/example/shelastudio/ui/create/CreateFragment.kt
@@ -0,0 +1,34 @@
+package com.example.shelastudio.ui.create
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shelastudio.databinding.FragmentCreateBinding
+
+class CreateFragment : Fragment() {
+
+    companion object {
+        private const val TAG = "CreateFragment"
+    }
+
+    private var _binding: FragmentCreateBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Log.d(TAG, "onCreateView")
+        _binding = FragmentCreateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
