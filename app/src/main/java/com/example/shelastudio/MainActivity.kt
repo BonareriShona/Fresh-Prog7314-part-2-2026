2976caf1fb7cf0b16225e8fa127565f3fd291815
Fanelo Maluleke <st10445045@imconnect.edu.za>
2026-09-17 17:20:28 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/shelastudio/MainActivity.kt
+++ app/src/main/java/com/example/shelastudio/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.shelastudio
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.shelastudio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG, "onCreate: MainActivity with bottom nav")
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        // Get the NavHostFragment and its controller
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Connect bottom nav to nav controller
+        binding.bottomNav.setupWithNavController(navController)
+        Log.d(TAG, "Bottom nav connected to nav controller")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
     }
 }
